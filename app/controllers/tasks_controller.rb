688b828e1e2bc0def40de3ad04a03a472f757e05
Thomas Cools <tacools10@gmail.com>
2016-11-22 15:58:17 +0100

    Progress 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,18 +15,21 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to task_path(@task)
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task.destroy
+
+    redirect_to tasks_path
   end
 
   private
@@ -33,4 +37,9 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:name, :description)
   end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
--- db/migrate/20161122100941_create_tasks.rb
+++ db/migrate/20161122100941_create_tasks.rb
@@ -4,6 +4,7 @@ class CreateTasks < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :description
 
+
       t.timestamps null: false
     end
   end
