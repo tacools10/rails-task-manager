2a88a2343b421cc421b9cb99c57724337478f79d
Thomas Cools <tacools10@gmail.com>
2016-11-22 13:45:43 +0100

    Progress

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
